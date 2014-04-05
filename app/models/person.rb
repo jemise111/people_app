63cad1170a54a6bf9e38df8b287978523c63c8b6
jemise111 <jesse.sessler@gmail.com>
2014-04-05 15:01:46 -0400

    create have_a_drink method and age helper method for person model

--- app/models/person.rb
+++ app/models/person.rb
@@ -8,4 +8,15 @@ class Person < ActiveRecord::Base
     birthdate.strftime("%D")
   end
 
+  def have_a_drink
+    self.drinks += 1 if age >= 21
+  end
+
+
+  private
+
+  def age
+    # does not account for leap years
+    (self.birthdate - DateTime.now).abs / (365 * 24 * 60 * 60)
+  end
 end
